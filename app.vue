bbd66fbdb154e76baf6cefeaa7a8bbf4b509e56e
Justin Arnold <justin.arnold@programmer.net>
2023-05-05 00:30:54 -0400

    update home page to use new api to add tasks

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 
-const {data: tasks } = await useFetch('/api/tasks')
+const {data: tasks, refresh } = await useFetch('/api/tasks')
+
+const newTask = ref('')
+
+const addTask = async () => {
+    const { data } = await useLazyFetch('/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify({ name: newTask.value })
+    })
+    refresh()
+}
 
 </script>
 
@@ -9,8 +19,10 @@ const {data: tasks } = await useFetch('/api/tasks')
         <div>
             <input type="text" v-model="newTask" placeholder="Add a new task">
             <button @click="addTask">Add</button>
-            ---
-            {{ tasks }}
+            <br/>
+            <div v-for="each in tasks" :key="each.id">
+                {{ each.name }}
+            </div>
         </div>
     </div>
 </template>
