2ab428fb240f4d71ea56c7711e2bddaf2e340e02
Justin Arnold <justin.arnold@programmer.net>
2023-05-06 15:28:38 -0400

    add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,11 @@ export default defineNuxtConfig({
         'nuxt-windicss',
         '@nuxtjs/supabase'
     ],
+    css: [
+        "primevue/resources/themes/lara-light-blue/theme.css",
+        "primevue/resources/primevue.css",
+    ],
+    build: {
+        transpile: ["primevue"]
+    }
 })
