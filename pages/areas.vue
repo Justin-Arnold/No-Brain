9eceac574372e72fa38c7c265cdc80d6a1a9215c
Justin Arnold <justin.arnold@programmer.net>
2023-09-02 18:19:45 -0400

    feat: create a basic page for areas

---
+++ pages/areas.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+    middleware: "authentication",
+    layout: "app-layout",
+});
+</script>
+<template>
+    <h1 class="text-white">Areas</h1>
+</template>
