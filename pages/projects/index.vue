dd011056c26850b4c30fb3e9ae13ee85c0c6211a
Justin Arnold <justin.arnold@programmer.net>
2023-09-02 23:59:10 -0400

    feat(projects): Add create project button

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Menubar from 'primevue/menubar';
 
 const user = useSupabaseUser();
 
@@ -12,32 +13,39 @@ const {
     query: { id: userID },
 });
 
-function createArea() {
+function createProject() {
     console.log('new')
 }
+
+
 
 </script>
 
 
 <template>
-    <div v-if="!!projects?.length" class="grid grid-cols-auto-fill-200 lg:grid-cols-auto-fill-300 gap-4">
-        <BaseCard v-for="project, index in projects" :key="index" class="aspect-square" :pt="{ body: { class: 'flex flex-col h-full'}, content: { class: 'grow'}}">
-            <template #title>
-                {{ project.name}}
-            </template>
-            <p>description text</p>
-            <template #footer >
-                <div class="flex gap-2 justify-end">
-                    <BaseButton label="Edit" text></BaseButton>
-                    <BaseButton label="Enter" @click="navigateTo('projects/' + project.id + '?id=' + userID)"></BaseButton>
-                </div>
-            </template>
-        </BaseCard>
-    </div>
-    <div v-else class="h-full grid place-items-center">
-        <div class="grid place-items-center gap-4">
-            <h2 class="text-white">No areas yet.</h2>
-            <BaseButton label="Create Area" text-only @click="createArea()"/>
+    <div class="relative h-full">
+        <div v-if="!!projects?.length" class="grid grid-cols-auto-fill-200 lg:grid-cols-auto-fill-300 gap-4">
+            <BaseCard v-for="project, index in projects" :key="index" class="aspect-square" :pt="{ body: { class: 'flex flex-col h-full'}, content: { class: 'grow'}}">
+                <template #title>
+                    {{ project.name}}
+                </template>
+                <p>description text</p>
+                <template #footer >
+                    <div class="flex gap-2 justify-end">
+                        <BaseButton label="Edit" text></BaseButton>
+                        <BaseButton label="Enter" @click="navigateTo('projects/' + project.id + '?id=' + userID)"></BaseButton>
+                    </div>
+                </template>
+            </BaseCard>
+        </div>
+        <div v-else class="h-full grid place-items-center">
+            <div class="grid place-items-center gap-4">
+                <h2 class="text-white">No areas yet.</h2>
+                <BaseButton label="Create Area" text-only @click="createArea()"/>
+            </div>
+        </div>
+        <div class="absolute bottom-0 left-1/2 -translate-x-1/2">
+            <BaseButton label="Create Project" text-only @click="createProject()"/>
         </div>
     </div>
 </template>
