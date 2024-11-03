f3ca40dee3097dfe756320a4a811b06e10a085bf
Justin Arnold <justin.arnold@programmer.net>
2024-11-03 14:34:34 -0500

    feat: Update styling of login page

--- apps/frontend/components/AuthCardLogin.vue
+++ apps/frontend/components/AuthCardLogin.vue
@@ -61,46 +61,26 @@ const onSubmit = handleSubmit(async (values) => {
 </script>
 
 <template>
-    <BaseCard class="w-[400px]">
+    <BaseCard class="w-[400px] flex flex-col">
         <template #title>
             <div class="user-select-none flex cursor-pointer items-center gap-4" @click="navigateTo('/')">
                 <img src="~/assets/images/logo.png" class="h-12" />
                 <h1 class="text-4xl font-bold text-white">Planote</h1>
             </div>
         </template>
-        <form class="flex flex-col gap-2 text-white" @submit.prevent>
+        <form class="flex flex-col gap-8 text-white my-8" @submit.prevent>
             <div class="flex flex-col">
-                <label for="plcGroupName"> Email </label>
-                <BaseInputText
-                    id="plcGroupEmail"
-                    v-bind="email"
-                    class="rounded p-1"
-                />
-                <small id="text-error" class="p-error p-0">{{
-                    errors.email || "&nbsp;"
-                }}</small>
+                <BaseInputText name="plcGroupName" label="Email" v-bind="email" :errors="errors.email" />
             </div>
             <div class="flex flex-col">
-                <label for="plcGroupName"> Password </label>
-                <BaseInputText
-                    id="plcGroupPassword"
-                    type="password"
-                    v-bind="password"
-                    class="rounded p-1"
-                />
-                <small id="text-error" class="p-error">{{
-                    errors.password
-                }}</small>
+                <BaseInputText name="plcGroupPassword" label="Password" type="password" v-bind="password"
+                    :errors="errors.password" />
                 <!-- <div class="flex items-center gap-2 text-slate-300 text-xs -mb-4 mt-2"><input type="checkbox" name="remember" value="true" />Remember Me</div> -->
             </div>
         </form>
         <template #footer>
-            <BaseButton
-                @click="onSubmit"
-                class="w-full"
-                :label="cardMode === 'login' ? 'Login' : 'Sign Up'"
-                :loading="isLoginPending"
-            />
+            <BaseButton @click="onSubmit" class="w-full" :label="cardMode === 'login' ? 'Login' : 'Sign Up'"
+                :loading="isLoginPending" />
             <p class="mt-2 text-center text-xs font-light text-slate-300">
                 Don't have an account? Click
                 <!-- <span class="text-purple-400" @click="switchMode">here</span> to -->
--- apps/frontend/components/BaseInputText.vue
+++ apps/frontend/components/BaseInputText.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-import InputText from "primevue/inputtext";
 import BaseLabel from "./BaseLabel.vue";
 
 defineProps<{
     label?: string
     name?: string
+    errors?: [] | string
+    type?: string
 }>()
 
 const value = defineModel<string>()
 </script>
 
 <template>
-    <BaseLabel :for="name" :label="label">
-        <InputText :id="name" :name="name" class="w-full" v-model="value" />
-    </BaseLabel>
+    <div class="flex flex-col gap-2">
+        <BaseLabel :for="name" :label="label">
+            <InputText :id="name" :name="name" :invalid="!!errors" class="w-full" v-model="value" :type="type" />
+        </BaseLabel>
+        <Message v-if="errors" severity="error" variant="simple" size="small">
+            {{ errors }}
+        </Message>
+    </div>
 </template>
