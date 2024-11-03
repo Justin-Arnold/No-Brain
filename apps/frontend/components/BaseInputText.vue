450d3bb0b2f1942f690049a51b75c25014055215
Justin Arnold <justin.arnold@programmer.net>
2024-11-03 12:34:34 -0500

    feat: Update a few components

--- apps/frontend/components/BaseInputText.vue
+++ apps/frontend/components/BaseInputText.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import InputText from "primevue/inputtext";
+import BaseLabel from "./BaseLabel.vue";
+
+defineProps<{
+    label?: string
+    name?: string
+}>()
+
+const value = defineModel<string>()
 </script>
 
 <template>
-    <InputText class="w-full" />
+    <BaseLabel :for="name" :label="label">
+        <InputText :id="name" :name="name" class="w-full" v-model="value" />
+    </BaseLabel>
 </template>
---
+++ apps/frontend/components/BaseLabel.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+defineProps<{
+    for?: string
+    label?: string
+}>()
+</script>
+
+<template>
+    <FloatLabel variant="in">
+        <slot></slot>
+        <label :for="for">
+            {{ label }}
+        </label>
+    </FloatLabel>
+</template>
