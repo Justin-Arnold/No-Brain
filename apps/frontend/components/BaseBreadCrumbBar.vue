35b91bfd176c1afa368ab9c13abd7af13cc004e5
Justin Arnold <justin.arnold@programmer.net>
2024-11-06 23:20:30 -0500

    fix: Update breadcrumb logic to work better

--- apps/frontend/components/BaseBreadCrumbBar.vue
+++ apps/frontend/components/BaseBreadCrumbBar.vue
@@ -5,7 +5,7 @@ enum RootNodeType {
 
 interface Node<T extends string = string> {
     label: T
-    path: string
+    route: string
 }
 
 type NavItem = {
@@ -27,10 +27,11 @@ const rootNode = ref<RootNode>()
 const additionalNodes = ref<Node[]>([])
 
 watchEffect(async () => {
+    console.log('tt', fullPath.value)
     if (fullPath.value[0].toLowerCase() === RootNodeType.PROJECT.toLowerCase()) {
         rootNode.value = {
             label: RootNodeType.PROJECT,
-            path: RootNodeType.PROJECT.toLowerCase()
+            route: `/${RootNodeType.PROJECT.toLowerCase()}`
         }
 
         if (fullPath.value[1]) {
@@ -45,8 +46,10 @@ watchEffect(async () => {
 
             additionalNodes.value = [{
                 label: project.value.name,
-                path: project.value.id
+                route: project.value.id
             }]
+        } else {
+            additionalNodes.value = []
         }
     }
 })
@@ -62,7 +65,13 @@ const allNodes = computed((): any[] => {
             root: {
                 class: '!bg-transparent'
             }
-        }" />
+        }">
+            <template #item="{ item }">
+                <NuxtLink :to="item.route" class="text-gray-400">
+                    {{ item.label }}
+                </NuxtLink>
+            </template>
+        </Breadcrumb>
         <div class="flex gap-2">
             <NuxtLink v-for="item in navItems" :key="item.label" class="p-2 text-gray-400" :to="item.path"
                 active-class="!text-primary-400">
