d6616da6511237de8c8f2d548cec17bca9a80a30
Justin Arnold <justin.arnold@programmer.net>
2024-11-09 23:29:53 -0500

    feat: Update overview page UI

--- apps/frontend/pages/projects/[projectId]/overview.vue
+++ apps/frontend/pages/projects/[projectId]/overview.vue
@@ -32,15 +32,14 @@ onBeforeMount(async () => {
 
 <template>
     <div class="h-full overflow-hidden flex flex-col">
-        <div class="flex justify-between gap-4">
-            <div class="flex items-center justify-between">
+        <div class="flex justify-between gap-4 h-full">
+            <div class="flex flex-col items-center justify-between">
                 <div class="flex flex-col items-start gap-2">
                     <BaseTag :label="areaName" />
                     <h1 class="text-4xl">{{ project?.name }}</h1>
                     <span class="text-lg">{{ project?.description }}</span>
                 </div>
             </div>
-            <hr class="my-4 border-none" />
             <div class="bg-surface-800 h-full w-[500px] p-4 rounded-md flex flex-col">
                 <div class="flex gap-2 items-center">
                     <h2 class="text-lg font-medium">
@@ -53,7 +52,7 @@ onBeforeMount(async () => {
                     </span>
                 </div>
                 <hr class="border-t-surface-700" />
-                <div>
+                <div class="flex flex-col gap-2">
                     <div v-for="milestone in project!.milestone" class="flex flex-col gap-1 bg-surface-900 rounded p-2">
                         <span class="font-semibold">{{ milestone.name }}</span>
                         <span class="text-gray-400">{{ milestone.description }}</span>
