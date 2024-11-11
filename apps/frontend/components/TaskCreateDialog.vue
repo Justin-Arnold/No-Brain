c35c53c105bfd2f2ac436d599cefcbf837209740
Justin Arnold <justin.arnold@programmer.net>
2024-11-10 23:19:56 -0500

    feat: Update and add components

---
+++ apps/frontend/components/TaskCreateDialog.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const isOpen = defineModel<boolean>("open");
+</script>
+
+<template>
+    <BaseDialogPrompt
+        v-model:open="isOpen"
+        title="Create a task"
+        type="text"
+        label="Task Name"
+    />
+</template>
