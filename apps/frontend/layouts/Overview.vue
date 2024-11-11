d545c88c85aaebab842b815bb7e8373e4c3a53cd
Justin Arnold <justin.arnold@programmer.net>
2024-11-10 20:25:35 -0500

    refactor: Refactor overview layout

--- apps/frontend/layouts/Overview.vue
+++ apps/frontend/layouts/Overview.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="h-full overflow-hidden">
-        <div class="flex justify-between gap-4 h-full">
-            <div class="flex flex-col items-center justify-between w-full">
-                <div class="w-full flex items-center justify-between">
+        <div class="flex h-full justify-between gap-4">
+            <div class="flex w-full flex-col items-center justify-between">
+                <div class="flex w-full items-center justify-between">
                     <div class="flex flex-col items-start gap-2">
                         <slot name="header" />
                     </div>
-                    <div>
+                    <div class="flex gap-2">
                         <slot name="header_action-bar" />
                     </div>
                 </div>
             </div>
-            <div v-if="$slots.panel" class="bg-surface-800 h-full w-[500px] p-4 rounded-md flex flex-col">
+            <div
+                v-if="$slots.panel"
+                class="flex h-full w-[500px] flex-col rounded-md bg-surface-800 p-4"
+            >
                 <slot name="panel" />
             </div>
         </div>
