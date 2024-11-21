d6df30a9974836aa9f5ccc76d424867638e8a901
Justin Arnold <justin.arnold@programmer.net>
2024-11-21 01:04:19 -0500

    feat: Restructure pages

--- apps/frontend/pages/projects/[projectId]/milestones.vue
+++ apps/frontend/pages/projects/[projectId]/milestones.vue
@@ -1,3 +1,21 @@
 <template>
-    <RouterView></RouterView>
+    <!-- <div class="h-full overflow-hidden grid grid-cols-3 gap-4">
+        <BaseCard v-for="milestone in milestones" :key="milestone.id" class="aspect-square"
+            :pt="{ body: { class: 'flex flex-col h-full' }, content: { class: 'grow' } }">
+            <template #title>
+                {{ milestone.name }}
+            </template>
+<p>
+    {{ milestone.description }}
+</p>
+<template #footer>
+                <div class="flex gap-2 justify-end">
+                    <BaseButton label="Edit" text></BaseButton>
+                    <BaseButton label="Enter" @click="navigateTo(`milestones/` + milestone.id)">
+                    </BaseButton>
+                </div>
+            </template>
+</BaseCard>
+</div> -->
+    <RouterView />
 </template>
--- apps/frontend/pages/projects/[projectId]/overview.vue
+++ apps/frontend/pages/projects/[projectId]/overview.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 definePageMeta({
     middleware: "authentication",
+    layout: "app-layout",
 });
 
 const route = useRoute();
