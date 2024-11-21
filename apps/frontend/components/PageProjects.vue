179c6eaa138d261407c9fd6028d947b483fffcb5
Justin Arnold <justin.arnold@programmer.net>
2024-11-21 01:04:35 -0500

    feat: Additional changes

---
+++ apps/frontend/components/PageProjects.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import type { NavItem } from './BaseBreadCrumbBar.vue';
+import type { Project } from '~/types/Project.types'
+
+defineProps<{
+    pageNavigationItems: NavItem[]
+    projects?: Project[]
+}>();
+
+</script>
+
+<template>
+    <NuxtLayout name="card-list-layout">
+        <BaseCard v-for="project in projects" :key="project.id" class="aspect-square"
+            :pt="{ body: { class: 'flex flex-col h-full' }, content: { class: 'grow' } }">
+            <template #title>
+                {{ project.name }}
+            </template>
+            <p>
+                {{ project.description }}
+            </p>
+            <template #footer>
+                <div class="flex gap-2 justify-end">
+                    <BaseButton label="Edit" text></BaseButton>
+                    <BaseButton label="Enter" @click="navigateTo('projects/' + project.id)"></BaseButton>
+                </div>
+            </template>
+        </BaseCard>
+        <!-- <template #bottomButton>
+            <BaseButton label="Create Project" text-only @click="createProjectModalIsOpen = true" />
+        </template>
+        <ProjectCreateDialog v-model="createProjectModalIsOpen" /> -->
+    </NuxtLayout>
+</template>
--- apps/frontend/layouts/appLayout.vue
+++ apps/frontend/layouts/appLayout.vue
@@ -1,8 +1,13 @@
+<script lang="ts" setup>
+const store = usePageNavigationStore()
+</script>
+
 <template>
     <div class="fixed left-0 top-0 flex h-screen w-screen">
         <AppSidebar></AppSidebar>
         <Toast />
-        <div class="w-full grow overflow-hidden p-8">
+        <div class="w-full grow overflow-hidden p-8 flex flex-col gap-8">
+            <BaseBreadCrumbBar :nav-items="store.navItems" />
             <slot />
         </div>
     </div>
